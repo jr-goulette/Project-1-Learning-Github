99d329951a91b8f6047b681453eff833b8feeed7
jr-goulette <goulettejr@vcu.edu>
2025-01-27 09:59:29 -0500

    Hated the color yellow and all of those helpful comments so I reverted back to the original plot/script

--- mtcars-figure.R
+++ mtcars-figure.R
@@ -2,22 +2,14 @@
 
 #Purpose of file: Learning Git and Github through the mtcars data set
 
-#Loading Useful Libraries
 library(tidyverse)
 library(ggplot2)
 
-#Viewing the mtcars data set, looking at the information page, and inspecting the 
-#type of variables
 View(mtcars)
 
 ?mtcars  
 
 str(mtcars)
 
-#Creating a plot
 ggplot(mtcars, aes(hp, mpg)) +
-  geom_point(color = "yellow") +
-  xlab("Horsepower") +
-  ylab("Miles/Gallon") +
-  theme_bw() + 
-  ggtitle("Horsepower and its Relationship to MPG")
+  geom_point()
