8e03856664d76bcb4fdd44bad9e84cada4442cd4
jr-goulette <goulettejr@vcu.edu>
2025-01-27 09:51:17 -0500

    adding comments

--- mtcars-figure.R
+++ mtcars-figure.R
@@ -2,15 +2,19 @@
 
 #Purpose of file: Learning Git and Github through the mtcars data set
 
+#Loading Useful Libraries
 library(tidyverse)
 library(ggplot2)
 
+#Viewing the mtcars data set, looking at the information page, and inspecting the 
+#type of variables
 View(mtcars)
 
 ?mtcars  
 
 str(mtcars)
 
+#Creating a plot
 ggplot(mtcars, aes(hp, mpg)) +
   geom_point() +
   xlab("Horsepower") +
